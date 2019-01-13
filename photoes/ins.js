167531eefb434e15f252aa4a3967e02efe75d4bf
SunnyLy <450512747@qq.com>
2019-01-13 17:38:20 +0800

    Site updated: 2019-01-13 17:38:19

--- photoes/ins.js
+++ photoes/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/SunnyLy/sunnyPhotoes/tree/master/sunnyPhotoes/min_photoes/' + data.link[i] + '.min.jpg';
-          var src = 'https://github.com/SunnyLy/sunnyPhotoes/tree/master/sunnyPhotoes/photoes/' + data.link[i];
+          var minSrc = 'https://github.com/SunnyLy/sunnyPhotoes/tree/master/sunnyPhotoes/min_photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://github.com/SunnyLy/sunnyPhotoes/tree/master/sunnyPhotoes/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
